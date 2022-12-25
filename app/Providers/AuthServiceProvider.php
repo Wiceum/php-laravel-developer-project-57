e48a7f23af361b79a3c503774e89f7c334e0de23
tamerlan volhin <wiceum@gmail.com>
2022-12-25 20:31:49 +0500

    Add authorization rule to change statuses

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('change-statuses', function ($user) {
+           return \Auth::check();
+        });
     }
 }
