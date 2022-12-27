98d62a1115cdc903edd1fd44cbe7cc912ca77db8
tamerlan volhin <wiceum@gmail.com>
2022-12-27 17:29:47 +0500

    Fix button styling

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -8,13 +8,12 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <!-- Scripts -->
         <!-- @Vite -->
     </head>
     <body>
-        <div class="font-sans text-gray-900 antialiased">
+        <div class="text-gray-900 antialiased">
             {{ $slot }}
         </div>
     </body>
--- resources/views/main_page.blade.php
+++ resources/views/main_page.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <title>Менеджер задач</title>
+    <title>{{__('TEST')}}</title>
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 <body>
