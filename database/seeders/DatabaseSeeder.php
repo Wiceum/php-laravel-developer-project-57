cbc0eeab163c2bea9dd523c50b1ce7ca7f4a3440
tamerlan volhin <wiceum@gmail.com>
2023-01-09 18:38:16 +0500

    Add testing task in seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,12 +28,19 @@ class DatabaseSeeder extends Seeder
         TaskStatus::create(['name' => 'в архиве']);
 
         User::create([
-            'name' => 'test',
-            'email' => 'test@example.com',
+            'name' => 'test_user',
+            'email' => 'test_user@example.com',
             'password'=>'$2y$10$ZmpI3LXaNELSDspCoCpJ/ePtFJAHGR0weAlKFT7mfs7C4ox2CvT1y',
             'email_verified_at' => now(),
             'remember_token' => Str::random(10)]
         );
 
+        Task::create([
+            'name' => 'First test task',
+            'description' => 'Any test description',
+            'status_id' => 1,
+            'created_by_id' => 1,
+            'assigned_to_id' => 1
+        ]);
     }
 }
