12ee54cda495e59e922d465ee5acd287a6cd1efb
tamerlan volhin <wiceum@gmail.com>
2023-01-09 15:06:01 +0500

    Create TasksTest.php

---
+++ tests/Feature/TasksTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Task;
+Use App\Models\User;
+
+class TasksTest extends TestCase
+{
+    use WithFaker;
+    use RefreshDatabase;
+
+
+}
