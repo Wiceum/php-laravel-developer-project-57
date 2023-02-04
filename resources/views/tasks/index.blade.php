24e7677904d8237a1cca50576d8a765c0d2f5a70
tamerlan volhin <wiceum@gmail.com>
2023-02-04 11:37:13 +0500

    Fix date format in tasks.index and labels.index

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -46,7 +46,7 @@
                 </td>
                 <td>{{ $task->author->name }}</td>
                 <td>{{ $task->executor ? $task->executor->name : '' }}</td>
-                <td>{{ $task->created_at }}</td>
+                <td>{{ $task->created_at->format('d.m.Y') }}</td>
                 @auth()
                 <td>
                     <a href="{{ route('tasks.edit', ['task' => $task]) }}"
