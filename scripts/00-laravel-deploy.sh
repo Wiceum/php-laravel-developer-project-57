bfc02c3e2292ec12541f5f2d9f1063a0d4412a9b
tamerlan volhin <wiceum@gmail.com>
2023-03-08 23:23:24 +0500

    Add Faker

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 echo "Running composer"
-composer install --working-dir=/var/www/html
+composer install --no-dev --working-dir=/var/www/html
+composer install fakerphp/faker
 
 echo "Caching config..."
 php artisan config:cache
