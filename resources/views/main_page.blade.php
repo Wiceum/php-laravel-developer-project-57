3e9f4fcb68e3eb8952b34c7060afa449fec5150b
tamerlan volhin <wiceum@gmail.com>
2023-03-09 16:55:46 +0500

    Use env() function instead of $_ENV

--- resources/views/main_page.blade.php
+++ resources/views/main_page.blade.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="csrf-param" content="_token" />
-    <link href="{{ asset('css/app.css', $_ENV['IS_ASSETS_SECURE']) }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css', env('IS_ASSETS_SECURE')) }}" rel="stylesheet">
     <title>{{__(config('app.name'))}}</title>
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    <script defer src="{{ asset('js/app.js', $_ENV['IS_ASSETS_SECURE']) }}"></script>
+    <script defer src="{{ asset('js/app.js', env('IS_ASSETS_SECURE')) }}"></script>
 </head>
 <body>
 <div id="app">
